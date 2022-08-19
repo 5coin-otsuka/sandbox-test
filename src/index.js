1bc4b1b82f3d8ddc426f1a58caf149db704f7256
5coin-otsuka <72871721+5coin-otsuka@users.noreply.github.com>
2022-08-19 16:03:39 +0900

    mapまで実施

--- src/index.js
+++ src/index.js
@@ -99,13 +99,78 @@
 // const message2 = `名前は${name}です。年齢は${age}歳です。`;
 // console.log(message2);
 
-const myProfile = ["じゃけぇ", 28];
+// const myProfile = ["じゃけぇ", 28];
 
-// 配列の場合
-const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
-console.log(message3);
+// // 配列の場合
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+// console.log(message3);
 
-// 分割代入：配列の場合
-const [name, age] = myProfile;
-const message4 = `名前は${name}です。年齢は${age}歳です。`;
-console.log(message4);
+// // 分割代入：配列の場合
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message4);
+
+/**
+ * デフォルト値、引数など
+ */
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん！`);
+// sayHello();
+
+/**
+ *　　スプレッド構文 ...
+ */
+
+//  配列の展開
+// const arr1 = [1, 2];
+// // console.log(arr1);
+// // console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+// // まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// // 分割代入で配列をまとめる
+// const [num1, num2, ...arr3] = arr2;
+// // 1
+// console.log(num1);
+// // 2
+// console.log(num2);
+// // [3, 4, 5]
+// console.log(arr3);
+
+// 配列のコピー、結合
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+
+// const arr6 = [...arr4];
+// arr6[0] = 100;
+// console.log(arr6);
+// console.log(arr４);
+// // [10, 20]
+// const arr6 = [...arr4];
+// console.log(arr6);
+
+// // [10, 20, 30, 40]
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+// const arr8 = arr4;
+// arr8[0] = 100;
+// console.log(arr4, arr8);
+/**
+ *map　や filterを使った配列の処理
+ */
+// 従来はfor文を利用していた
+const nameArr = ["田中", "山田", "じゃけぇ"];
+for (let index = 0; index < nameArr.length; index++) {
+  console.log(nameArr[index]);
+}
+
+const nmaeArr2 = nameArr.map((name) => {
+  return name;
+});
+console.log(nmaeArr2);
+
+nameArr.map((name) => console.log(name));
